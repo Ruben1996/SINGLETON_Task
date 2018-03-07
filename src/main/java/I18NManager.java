424e82a16990aa19deeef65fbf278b68917ba869
Ruben1996 <rubencobe96@gmail.com>
2018-03-07 16:34:43 +0100

    src

---
+++ src/main/java/I18NManager.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+
+public class I18NManager {
+    private static I18NManager instance;
+    private HashMap<String, ResourceBundle> data;
+}
